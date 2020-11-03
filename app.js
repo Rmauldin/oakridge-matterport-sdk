5b10a5f1b8cf2432cef2b54c3cde8594a42ebf12
rmauldin <rmauldin@matterport.com>
2020-11-03 15:45:10 -0800

    Mattertag Gallery added

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const sdkVersion = '3.5';
 const params = "&play=1";
 const menuParams = "&play=1&qs=1";
 const initSpace = "YJx1weuenGk";
-const galleryDistance = 15; // meters
+const galleryDistance = 10; // meters
 const spaces = {
     "Elementary School": "YJx1weuenGk", 
     "Middle School": "MZuYopSBizg", 
     "Lunch Area And Playground": "TvkHYt3AYKe", 
     "Preschool": "ZMS7QHBGaDH"
 };
-let arrow, title, menu, iframe, modal;
+let arrow, title, menu, iframe, modal, gallery_container;
 // const school_regex = /\((https?:\/\/(?:www\.)?oakridgeschool.org\/?.*?)\)/;
 
 document.addEventListener('DOMContentLoaded', ()=>{
     arrow = document.getElementById('space-menu-toggle');
     title = document.getElementById('menu-title');
     menu = document.getElementById('space-menu');
-
+    gallery_container = document.getElementById('mattertag-gallery-container');
     iframe = document.getElementById('showcase');
     iframe.setAttribute('src', `https://my.matterport.com/show/?m=${initSpace}${params}`);
     iframe.addEventListener('load', showcaseLoader, true);
@@ -43,14 +43,12 @@ function showcaseLoader(){
 async function loadedShowcaseHandler(sdk){
     console.debug('SDK Connected');
     const menu_container = document.getElementById('menu-container');
-    const gallery = document.getElementById('gallery');
+    const gallery = document.getElementById('mattertag-gallery');
     const observers = setupObservers();
     const tags = await setupTags();
     setupGallery(tags);
 
     // functions
-    // TODO populateGallery
-    // TODO getClosestImageTags
     
     function setupObservers(){
         const observers = {}
@@ -62,7 +60,7 @@ async function loadedShowcaseHandler(sdk){
         sdk.App.state.waitUntil(state => state.phase === 'appphase.starting')
         .then(() => {
             menu_container.style.display = 'flex';
-            gallery.style.display = 'flex';
+            gallery_container.style.display = 'flex';
         })
         .catch(console.error);
 
@@ -77,61 +75,78 @@ async function loadedShowcaseHandler(sdk){
 
         const tags = await sdk.Mattertag.getData();
         
-
         if(iframe.getAttribute('src').includes('emb=1')){
             sdk.on(sdk.Mattertag.Event.LINK_OPEN, (tagSid, url) => {
                 menu.classList.add('hidden');
                 title.classList.add('hidden');
-                arrow.children[0].style.transform = `rotate(315deg)`;
+                closeArrow(arrow);
             });
         }
 
-        return tags;
-
-    
+        return tags;    
     }
 
     function setupGallery(tags){
-        let imageTags = getClosestImageTags();
-        populateGallery(imageTags);
+        const arrow_hide = document.getElementById('mattertag-gallery-toggle');
+        setupArrowGallery(arrow_hide);
+
+        populateGallery(getClosestImageTags());
         sdk.on(sdk.Sweep.Event.ENTER, (oldID, newID) => {
-            imageTags = getClosestImageTags();
+            let imageTags = getClosestImageTags();
             populateGallery(imageTags);
         });
 
-        function populateGallery(imageTags){
+        async function populateGallery(imageTags){
             while(gallery.firstChild){
                 gallery.removeChild(gallery.firstChild);
             }
-            imageTags.forEach(tag => {
-                const cont = document.createElement('div');
-                const imgEle = document.createElement('img');
-                cont.insertAdjacentElement('beforeend', imgEle);
+            
+            imageTags.sort((a, b) => a.distance - b.distance)
+            .forEach(addEle);
 
-                imgEle.setAttribute('src', tag.media.src);
-                imgEle.setAttribute('alt', "");
+            function addEle(tag){
+                let img = new Image();
+                img.onload = () => {
+                    const _cont = document.createElement('div');
+                    _cont.setAttribute('id', tag.sid);
+                    _cont.setAttribute('class', 'mattertag-gallery-item');
+                    const imgEle = document.createElement('img');
+                    _cont.insertAdjacentElement('beforeend', imgEle);
+                    imgEle.setAttribute('src', tag.media.src);
+                    _cont.addEventListener('click', () => {
+                        sdk.Mattertag.navigateToTag(tag.sid, sdk.Mattertag.Transition.FLY);
+                    });
+                    gallery.insertAdjacentElement('beforeend', _cont);
+                }
+                img.src = tag.media.src;
+            }
 
-                cont.addEventListener('click', () => {
-                    sdk.Mattertag.navigateToTag(tag.sid, sdk.Mattertag.Transition.FLY);
-                });
-
-                gallery.insertAdjacentElement('beforeend', cont);
-            });
         }
-    
+        
         function getClosestImageTags(){
             if(!observers['pose']) return [];
-            return tags
-            .filter(tag => tag.media.type === 'photo')
-            .filter(tag => euclideanDistance3D(observers['pose'].position, tag.anchorPosition) <= galleryDistance);
+            let photoTags = tags.filter(tag => tag.media.type === 'photo');
+            photoTags.forEach(tag => {
+                tag.distance = euclideanDistance3D(observers['pose'].position, tag.anchorPosition);
+            });
+            return photoTags.filter(tag => tag.distance <= galleryDistance);
         }
 
-        function euclideanDistance3D(pos1, pos2){
-            return Math.sqrt( 
-                Math.pow(pos1.x - pos2.x, 2) +
-                Math.pow(pos1.y - pos2.y, 2) +
-                Math.pow(pos1.z - pos2.z, 2)
-             );
+        function setupArrowGallery(arr){
+            
+            arr.addEventListener('click', toggleHidden);
+
+            function toggleHidden(){
+                toggleArrow(arr.children[0]);
+                gallery.style.height = gallery.style.height === "0px" ? "130px" : "0px";
+                arr.style.top = arr.style.top === "0px" ? "130px" : "0px";
+            }
+
+            // function makeHidden(){
+            //     closeArrow(arr.children[0]);
+            //     gallery.style.height = "0px";
+            //     arr.style.top = "0px";
+            // }
         }
     }
 
@@ -142,8 +157,8 @@ function setupSpaceMenu(){
 
     if(spaces.length <= 0 ) return;
     const menu = document.getElementById('space-menu');
-    let listEle;
 
+    let listEle;
     for(const [name, sid] of Object.entries(spaces)){
         listEle = document.createElement('li');
         listEle.innerText = name;
@@ -163,23 +178,46 @@ function setupSpaceMenu(){
             title.classList.toggle('hidden');
             menu.classList.toggle('hidden');
 
-            let st = getComputedStyle(arrow.children[0]);
-            let tr =    st.getPropertyValue("-webkit-transform") ||
-                        st.getPropertyValue("-moz-transform") ||
-                        st.getPropertyValue("-ms-transform") ||
-                        st.getPropertyValue("-o-transform") ||
-                        st.getPropertyValue("transform");
-
-            let values = tr.split('(')[1];
-            values = values.split(')')[0];
-            values = values.split(',');
+            toggleArrow(arrow.children[0]);
             
-            let a = values[0],
-                b = values[1];
-
-            const angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-            const newAngle = angle + 180 % 360;
-            arrow.children[0].style.transform = `rotate(${newAngle}deg)`;
         });
+    }
+
+}
+
+// utility functions
+function euclideanDistance3D(pos1, pos2){
+    return Math.sqrt( 
+        Math.pow(pos1.x - pos2.x, 2) +
+        Math.pow(pos1.y - pos2.y, 2) +
+        Math.pow(pos1.z - pos2.z, 2)
+        );
+}
+
+function toggleArrow(arrowEle){
+    let st = getComputedStyle(arrowEle);
+    let tr =    st.getPropertyValue("-webkit-transform") ||
+                    st.getPropertyValue("-moz-transform") ||
+                    st.getPropertyValue("-ms-transform") ||
+                    st.getPropertyValue("-o-transform") ||
+                    st.getPropertyValue("transform");
+
+    let values = tr.split('(')[1];
+    values = values.split(')')[0];
+    values = values.split(',');
+    
+    let a = values[0],
+        b = values[1];
+
+    const angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    const newAngle = angle + 180 % 360;
+    arrowEle.style.transform = `rotate(${newAngle}deg)`;
+}
+
+function closeArrow(arrowEle){
+    if(arrowEle.style.transform === 'rotate(135deg)'){
+        arrowEle.style.transform = `rotate(315deg)`;
+    }else if(arrowEle.style.transform === 'rotate(225deg)'){
+        arrowEle.style.transform = `rotate(45deg)`;
     }
 }
